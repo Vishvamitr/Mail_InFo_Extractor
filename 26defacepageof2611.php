3a631eccfe69a6912a165439234f278a0428a6ca
Vishvamitr <73627176+Vishvamitr@users.noreply.github.com>
2020-11-28 13:47:19 +0530

    Create 26defacepageof2611.php

---
+++ 26defacepageof2611.php
@@ -0,0 +1,349 @@
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+   <link href='http://fonts.googleapis.com/css?family=Black+Ops+One' rel='stylesheet' type='text/css'>
+   <link href='https://fonts.googleapis.com/css?family=Ubuntu+Condensed' rel='stylesheet' type='text/css'>
+   <link href='http://fonts.googleapis.com/css?family=Orbitron' rel='stylesheet' type='text/css'> 
+   <link href='https://fonts.googleapis.com/css?family=Covered+By+Your+Grace' rel='stylesheet' type='text/css'>
+   <link href='https://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
+   <title>Hacked By GH057_5P3C706</title>
+	   <meta property="og:title" content="Tributes to martyrs of the 26/11 terror attack" />
+	   <meta property="og:image"  content="https://ibb.co/nxz3fk" /> 
+<style type="text/css">
+body {
+    background: #000000;
+    color: #e2e2e2;
+}
+	body { height: 100%; width: 100%; margin: 0; padding: 0; background-color: #000000; background-size: 100%; background-position: center; background-repeat: no-repeat; color: #FFFFFF; font-family: "Orbitron"; font-size: 14pt; text-align: center; text-decoration: none; } #greets { width: 720px; border-style: outset; } #footer { font-size: 9pt; opacity: .7; } .glow { text-shadow: 0 0 10px #F59700; font-family: "Black Ops One"; } .blueglow { text-shadow: 0 0 10px #0F6FBD; font-family: "Black Ops One" } .whiteglow { text-shadow: 0 0 10px #FFFFFF; font-family: "Black Ops One"; } 
+a{
+   color:green;
+}
+</style>
+   <style type="text/css"> 
+   body { 
+      background-image: url('https://i.imgur.com/QUN1nrk.gif');
+	background-repeat: no-repeat;
+    background-attachment: fixed;
+	  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+
+
+
+   background-color: #000000; 		
+   color: #FFFFFF; 	
+   font-family: "Orbitron"; 	
+   text-align: center; 	
+   text-decoration: none; 	} 	
+   #greets { 		width: 820px; 		border-style: outset; 	} 
+   #footer { 		font-size: 9pt; 		opacity: .7; 	} 	 	.glow { 	
+   text-shadow: 0 0 10px #F59700; 		font-family: "Black Ops One"; 	} 	 
+   .blueglow { 		text-shadow: 0 0 10px #0F6FBD; 		font-family: "Black Ops One" 	} 	 	
+   .whiteglow { 		text-shadow: 0 0 10px #FFFFFF; 		font-family: "Black Ops One"; 	}
+   </style>
+   
+</head>
+<body>
+<center>
+<pre>
+   <span class="glow" style="font-size: 30pt;"> Hacked By GH057_5P3C706</span>
+   <br>
+<font size="6">Tributes to martyrs of the </font><font color="red" size="5" face="Black Ops One">#26/11 terror attack</font>
+<br>
+<br>
+ <img src="https://static.asianetnews.com/images/01cx881fkwt4egn7gn298ry2d7/26-11-Mumbai-attacks.jpg" width="750" height="450" >
+
+<br>
+<font size="5">A Small Tribute to those Brave Soldiers who Laid their<br><br> Precious Life for our Country and our People !!
+<br>I Really Pity u guys .. U dOnt have People like Major Sandeep Unnikrishnan, Jaywant Patil <br><br>Prakash More, Vijay Salaskar, Ashok Kamte, Hemant Karkare and many mOre !!
+ <br><br>
+So According to u .. Patriotism = War = Jihaad = Shit
+ <br><br>
+ 
+ <font size="6" color="green" >============================================</font>
+ <br>
+WE ARE THE VOICE OF THE SILENT AND THE EYES OF THE BLIND.</font>
+
+<center><font size="5" >WE ARE LEGION | WE DO NOT FORGIVE | WE DO NOT FORGET | EXPECT US</font>
+
+   <font size="6" color="green" >============================================</font>
+   
+   <p>
+<font color="red" size="5" face="Orbitron">
+</font>
+</center>
+<CENTER><FONT size="6" face="Black Ops One">
+<SCRIPT>
+
+
+
+farbbibliothek = new Array(); 
+
+
+
+farbbibliothek[0] = new Array("#FF0000","#FF1100","#FF2200","#FF3300","#FF4400","#FF5500","#FF6600","#FF7700","#FF8800","#FF9900","#FFaa00","#FFbb00","#FFcc00","#FFdd00","#FFee00","#FFff00","#FFee00","#FFdd00","#FFcc00","#FFbb00","#FFaa00","#FF9900","#FF8800","#FF7700","#FF6600","#FF5500","#FF4400","#FF3300","#FF2200","#FF1100"); 
+
+
+
+farbbibliothek[1] = new Array("#00FF00","#000000","#00FF00","#00FF00"); 
+
+
+
+farbbibliothek[2] = new Array("#00FF00","#FF0000","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00"); 
+
+
+
+farbbibliothek[3] = new Array("#FF0000","#FF4000","#FF8000","#FFC000","#FFFF00","#C0FF00","#80FF00","#40FF00","#00FF00","#00FF40","#00FF80","#00FFC0","#00FFFF","#00C0FF","#0080FF","#0040FF","#0000FF","#4000FF","#8000FF","#C000FF","#FF00FF","#FF00C0","#FF0080","#FF0040"); 
+
+
+
+farbbibliothek[4] = new Array("#FF0000","#EE0000","#DD0000","#CC0000","#BB0000","#AA0000","#990000","#880000","#770000","#660000","#550000","#440000","#330000","#220000","#110000","#000000","#110000","#220000","#330000","#440000","#550000","#660000","#770000","#880000","#990000","#AA0000","#BB0000","#CC0000","#DD0000","#EE0000"); 
+
+
+
+farbbibliothek[5] = new Array("#000000","#000000","#000000","#FFFFFF","#FFFFFF","#FFFFFF"); 
+
+
+
+farbbibliothek[6] = new Array("#0000FF","#FFFF00"); 
+
+
+
+farben = farbbibliothek[4];
+
+
+
+function farbschrift() 
+
+
+
+{ 
+
+
+
+for(var i=0 ; i<Buchstabe.length; i++) 
+
+
+
+{ 
+
+
+
+document.all["a"+i].style.color=farben[i]; 
+
+
+
+} 
+
+
+
+farbverlauf(); 
+
+
+
+} 
+
+
+
+function string2array(text) 
+
+
+
+{ 
+
+
+
+Buchstabe = new Array(); 
+
+
+
+while(farben.length<text.length) 
+
+
+
+{ 
+
+
+
+farben = farben.concat(farben); 
+
+
+
+} 
+
+
+
+k=0; 
+
+
+
+while(k<=text.length) 
+
+
+
+{ 
+
+
+
+Buchstabe[k] = text.charAt(k); 
+
+
+
+k++; 
+
+
+
+} 
+
+
+
+} 
+
+
+
+function divserzeugen() 
+
+
+
+{ 
+
+
+
+for(var i=0 ; i<Buchstabe.length; i++) 
+
+
+
+{ 
+
+
+
+document.write("<span id='a"+i+"' class='a"+i+"'>"+Buchstabe[i] + "</span>"); 
+
+
+
+} 
+
+
+
+farbschrift(); 
+
+
+
+} 
+
+
+
+var a=1; 
+
+
+
+function farbverlauf() 
+
+
+
+{ 
+
+
+
+for(var i=0 ; i<farben.length; i++) 
+
+
+
+{ 
+
+
+
+farben[i-1]=farben[i]; 
+
+
+
+} 
+
+
+
+farben[farben.length-1]=farben[-1]; 
+
+
+
+
+
+
+
+setTimeout("farbschrift()",30); 
+
+
+
+} 
+
+
+
+var farbsatz=1; 
+
+
+
+function farbtauscher() 
+
+
+
+{ 
+
+
+
+farben = farbbibliothek[farbsatz]; 
+
+
+
+while(farben.length<text.length) 
+
+
+
+{ 
+
+
+
+farben = farben.concat(farben); 
+
+
+
+} 
+
+
+
+farbsatz=Math.floor(Math.random()*(farbbibliothek.length-0.0001)); 
+
+
+
+} 
+
+
+
+setInterval("farbtauscher()",5000); 
+
+
+
+text= "FEEL THE POWER INDIAN HACKERS"; //h 
+
+
+
+string2array(text); 
+
+
+
+divserzeugen(); 
+
+
+
+//document.write(text);</SCRIPT>
+</CENTER></FONT>
+<br>
+<font size="4">
+<p>
+<span style="color:#ff9933">GH057_R007 | 8L4CK P3RL | F0R81DD3N H4CX3R | RED LIZARD |<br> S3CU617Y_R1PP36 | R007_SH3LL | 4N0N 5P1D3R | </span><br><span style="color:#fff">N0VA C05M05 | 8L4CK_CH3V4L13R | BU6_M0NK3Y | N3TW0RK GH057 | C0D3_PH03N1X |<br>5H4D0W_HUN7ER | M@D C0D3 | C0D3 3XP3RT | D34D FR34K</span><br><span style="color:#26bf18">M3GA MIND | D0PP3l_64N63R | CH@CH_4-RC7 | K1LL3R C0BR4{PP} | 7R0J4N_9H057 | <br>4M4L HAX0R | J0N M4CCL0UD | J4K H4CK3R | COD3_CR4CK3R </span>
+</body>
+</html>
